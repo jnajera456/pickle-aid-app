94046e4e7ab2d297b2fbe8c44a0fc1f199c74a79
jesseN <jessenajera06@gmail.com>
2024-12-05 21:08:17 -0500

    created the basic format for the HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,25 @@
 import logo from '../pickle-logo.png';
 import '../styling sheets/HomePage.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <div className="App-header">
+        <div className="left-header">
+          <h1 className ="mainHeader">
+          Got Yourself In A Pickle? </h1>
+          <p className ="mainParagraph">
+          Find solutions to whatever’s weighing you down and reclaim your peace of mind! Explore tools designed to help you navigate college life with ease. </p>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Got Yourself in A Pickle?
-        </h1>
-        <p>
-          Look for your problem and get some advice!
-        </p>
-      </header>
+      </div>
+      <div className = "buttons">
+        <button className="buttonOne">Academic Stress</button>
+        <button className="buttonTwo">Emotional Well-being</button>
+        <button className="buttonThree">Social Challenges</button>
+        <button className="buttonFour">Life Balance</button>
+      </div>
     </div>
   );
 }
